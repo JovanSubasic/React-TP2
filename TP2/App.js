d8202fa3d86b8b8c6ba510a7329d8339b3ce2841
JovanSubasic <105287870+JovanSubasic@users.noreply.github.com>
2022-05-24 15:55:49 +0200

    24/05

--- TP2/App.js
+++ TP2/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View , Image} from 'react-native';
+import { StyleSheet, Text, View , Image , ActivityIndicator , FlatList} from 'react-native';
 import * as Location from 'expo-location';
 import React , {useState, useEffect} from 'react';
 
@@ -10,6 +10,7 @@ export default function App() {
   const [temperature, setTemperature] = useState(null);
   const [description, setDescritpion] = useState(null);
   const [icon, setIcon] = useState(null);
+  const [prevision, setPrevision] = useState(null);
 
   // +'.location.latitude.'+
 
@@ -29,13 +30,23 @@ export default function App() {
   // };
 
   const getWeather = () => {
-    fetch('https://api.openweathermap.org/data/2.5/weather?lat='+location.coords.latitude+'&lon='+location.coords.longitude+'&appid=4f13375416924547311654d5dd4a1363&units=metric&lang=fr ')
+    fetch('https://api.openweathermap.org/data/2.5/weather?lat='+location.coords.latitude+'&lon='+location.coords.longitude+'&appid=a3cca33b945358f4298ec5a580676d0f&units=metric&lang=fr ')
         .then(response => response.json())
         .then(data => {
           setTemperature(data.main.temp);
           setDescritpion(data.weather[0].description);
           setIcon(data.weather[0].icon);
           // alert(JSON.stringify(data))
+        })
+  }
+
+  const getPrevi = () => {
+    fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+location.coords.latitude+'&lon='+location.coords.longitude+'&exclude=hourly,minutely&appid=a3cca33b945358f4298ec5a580676d0f&units=metric&lang=fr ')
+        .then(response => response.json())
+        .then(data => {
+          setPrevision(data);
+          
+          // console.log(data)
         })
   }
  
@@ -57,26 +68,45 @@ export default function App() {
       // let result = getMoviesFromApiAsync();
       // setApi(result);
     })();
+
+    location && getWeather();
+    location && getPrevi();
   }, []);
  
   let text = 'Waiting..';
+  let attente = '';
   if (errorMsg) {
     text = errorMsg;
+    
   } else if (location) {
     text = location.coords.latitude;
+    attente = "ok";
     // let result = alert(getMoviesFromApiAsync());
-    text = getWeather();
+    
+
   }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.paragraph}>{text}</Text>
-      <Text style={styles.paragraph}>Température : {temperature} °C</Text>
-      <Text style={styles.paragraph}>Déscription : {description}</Text>
-      <Text style={styles.paragraph}>Icon : 
-      <Image style={{width: 30, height: 30}}
+      <Text style={styles.paragraph}>{"\n"}{"\n"}{"\n"}</Text>
+      {attente != ''? <Text style={styles.load}></Text>: <ActivityIndicator style={styles.load} color={"#385164"} /> }
+      {attente != ''? <Text style={styles.paragraph}>Température : {temperature} °C</Text>: null }
+      
+      {attente != ''? <Text style={styles.paragraph}> 
+      <Image style={styles.icon}
           source={{uri: 'http://openweathermap.org/img/wn/'+icon+'.png'}}
-        /></Text>
+        /></Text>: null }
+      {attente != ''? <Text style={styles.paragraph}>{description}</Text>: null }
+      {attente != ''? 
+      prevision && 
+      <FlatList
+          data={prevision.daily}
+          renderItem={({item}) => 
+          <Text style={styles.item}>{"\n"} Température : {item.temp.max} °C{"\n"}
+          <Image style={styles.icon}
+              source={{uri: 'http://openweathermap.org/img/wn/'+item.weather[0].icon+'.png'}}
+            />{"\n"}{item.weather[0].description}{"\n"}{"\n"}</Text>}
+        />: null }
     </View>
   );
 }
@@ -86,6 +116,14 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'top',
+  },
+  load: {
+    alignItems: 'center',
+  },
+  icon: {
+    width: 100,
+    height: 100,
+    alignItems: 'center',
   },
 });
